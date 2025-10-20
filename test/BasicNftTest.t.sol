aa58d92851e32a62dffc8b5e76ea735f680bdfdc
refaldy <refaldybanggana@gmail.com>
2025-10-20 22:14:32 +0700

    add tests

---
+++ test/BasicNftTest.t.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+import {Test} from "forge-std/Test.sol";
+import {BasicNFT} from "../src/BasicNFT.sol";
+import {DeployBasicNFT} from "../script/DeployBasicNft.s.sol";
+
+contract BasicNftTest is Test {
+    DeployBasicNFT public deployer;
+    BasicNFT public basicNft;
+    address public USER = makeAddr("user");
+    string public constant PUG =
+        "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+
+    function setUp() public {
+        deployer = new DeployBasicNFT();
+        basicNft = deployer.run();
+    }
+
+    function testNameIsCorrect() public view {
+        string memory expectedName = "Dogie";
+        string memory actualName = basicNft.name();
+        // assertEq(actualName, expectedName);
+        assert(
+            keccak256(abi.encodePacked(actualName)) ==
+                keccak256(abi.encodePacked(expectedName))
+        );
+    }
+
+    function testCanMintAndHaveBalance() public {
+        vm.prank(USER);
+        basicNft.mintNft(PUG);
+
+        assert(basicNft.balanceOf(USER) == 1);
+        assert(
+            keccak256(abi.encodePacked(basicNft.tokenURI(0))) ==
+                keccak256(abi.encodePacked(PUG))
+        );
+    }
+}
