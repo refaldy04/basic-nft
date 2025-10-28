ed6aebf1c3e586b7911744ddb6a4a1513dc856b7
refaldy <refaldybanggana@gmail.com>
2025-10-28 21:50:10 +0700

    modify the NFT image onchain

--- src/MoodNFT.sol
+++ src/MoodNFT.sol
@@ -5,6 +5,9 @@ import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
 
 contract MoodNFT is ERC721 {
+    // errors
+    error MoodNFT__CantFlipMoodIfNotOwner();
+
     uint256 private s_tokenCounter;
     string private s_sadSvgImageUri;
     string private s_happySvgImageUri;
@@ -29,6 +32,18 @@ contract MoodNFT is ERC721 {
         _safeMint(msg.sender, s_tokenCounter);
         s_tokenIdToMood[s_tokenCounter] = Mood.HAPPY;
         s_tokenCounter++;
+    }
+
+    function flipMood(uint256 tokenId) public {
+        // Only want the NFT owner to be able to change the mood
+        if (!_isApprovedOrOwner(msg.sender, tokenId)) {
+            revert MoodNFT__CantFlipMoodIfNotOwner();
+        }
+        if (s_tokenIdToMood[tokenId] == Mood.HAPPY) {
+            s_tokenIdToMood[tokenId] = Mood.SAD;
+        } else {
+            s_tokenIdToMood[tokenId] = Mood.HAPPY;
+        }
     }
 
     function _baseURI() internal pure override returns (string memory) {
